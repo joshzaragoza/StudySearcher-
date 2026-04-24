1342f6a174a26ff0f3e30e0d47e39082780d9fe4
Loihi77 <robloihi77@Loihi.localdomain>
2026-04-24 15:46:51 -0700

    Login.js made, html made to support login page

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # StudySearcher-
+#test test
---
+++ jsFiles/login.js
@@ -0,0 +1,27 @@
+const users = [ {uid: "000000001", password: "pass123", name: "robby"} ];
+
+function login() {
+    const uid = document.getElementById("uid").value;
+    const password = document.getElementById("password").value;
+    const message = document.getElementById("message");
+
+    if (uid === "" || password === "") {
+        message.textContent("Please fill in all fields.");
+        return;
+    }
+    
+    const foundUser = users.find(user => user.uid === uid && user.password === password);
+
+    if (foundUser) {
+        message.textContent = `Welcome, ${foundUser.name}!`;
+
+        localStorage.setItem("loggedInUser", foundUser.email);
+
+    // Later we can redirect pages here
+    // window.location.href = "dashboard.html";
+    
+    } else {
+        message.textContent = "Invalid UID or password.";
+    }
+
+}
