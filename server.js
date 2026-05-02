f01028251634583da468cf5ec7213af44c872064
Loihi77 <robloihi77@Loihi.localdomain>
2026-05-01 22:01:28 -0700

    got database set up, sign up page set up, connected front end to back end on signup page

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/', authRoutes);
 
