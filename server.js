d6482bbb8a239f43dda61a20ed1c12dafcfab95d
daanniill <daniil.malakhov12@gmail.com>
2026-05-10 19:02:35 -0700

    linked auth routes in server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/users", require("./routes/users"));
-
+app.use("/api/auth", require("./routes/auth"));
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
