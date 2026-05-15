c221696a7df555e56495a915f84853c3e6fb7581
Loihi77 <robloihi77@Loihi.localdomain>
2026-05-14 20:38:22 -0700

    add home page, profile page, button moving from home to profile and finally a logout button

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -21,9 +21,8 @@ function LoginPage() {
             setMessage(data.message);
 
             if (res.ok) {
-                localStorage.setItem("loggedInUser", uid);
-                // Redirect to dashboard or another page
-                // window.location.href = "/dashboard";
+                localStorage.setItem("loggedInUser", JSON.stringify(data.user));
+                window.location.href = "/home";
             }
         } catch (error) {
             setMessage("An error occurred during login. Please try again later.");
