734f91b15ffbbb350b1c8d6bb298e35144a418b9
Joshua Zaragoza <joshuazaragoza@ucla.edu>
2026-05-22 00:02:41 -0700

    Add matchmaking by shared classes; availability matching pending

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(cors());
 
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
+app.use("/api/matches", require("./routes/matches"));
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
