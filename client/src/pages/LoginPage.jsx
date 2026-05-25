1bcf5fe1b290cce361ab69695582fb47231f5c9a
daanniill <daniil.malakhov12@gmail.com>
2026-05-24 18:45:48 -0700

    added classname to log in button and cleaned up indentation in LoginPage.jsx

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -30,37 +30,36 @@ function LoginPage() {
     }
 
       return (
-                <div className="auth-container">
-                    <form className="auth-box" onSubmit={handleLogin}>
-                        <h1>Log In</h1>
+        <div className="auth-container">
+            <form className="auth-box" onSubmit={handleLogin}>
+                <h1>Log In</h1>
 
-                        <input
-                        placeholder="UID"
-                        value={uid}
-                        onChange={(e) => setUid(e.target.value)}
-                        />
+                <input
+                    placeholder="UID"
+                    value={uid}
+                    onChange={(e) => setUid(e.target.value)}
+                />
 
-                        <input
-                            type="password"
-                            placeholder="Password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
 
-                        <button type="submit">Log In</button>
+                <button className="btn btn--primary" type="submit">Log In</button>
 
-                        <p>{message}</p>
+                <p>{message}</p>
 
-                        <p
+                <p
+                    className="auth-link"
+                    onClick={() => {window.location.href = "/signup"}}
+                >
 
-                        className="auth-link"
-                        onClick={() => {window.location.href = "/signup"}}
-                        >
-
-                            Don't have an account? Sign Up
-                        </p>
-                    </form>
-                </div>
+                    Don't have an account? Sign Up
+                </p>
+            </form>
+        </div>
      );
     }
 
