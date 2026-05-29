04130253576e73250487a9ee2d38b3b0a38474a5
daanniill <daniil.malakhov12@gmail.com>
2026-05-28 20:14:25 -0700

    Added Lost&Found link to navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,6 +7,7 @@ function NavBar() {
                 <a href="/matches" className="navbar-link">Matches</a>
                 <a href="/messages" className="navbar-link">Messages</a>
                 <a href="/profile" className="navbar-link">Profile</a>
+                <a href="/lost-found" className="navbar-link">Lost & Found</a>
                 <button
                     className="btn btn--danger"
                     onClick={() => {
