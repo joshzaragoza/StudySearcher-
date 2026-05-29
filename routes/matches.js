2e720e662238f19a41d4065e598b8024ecf02180
Joshua Zaragoza <joshuazaragoza@ucla.edu>
2026-05-29 16:47:48 -0700

    Add availability overlap to matchmaking

--- routes/matches.js
+++ routes/matches.js
@@ -12,16 +12,40 @@ router.get("/:id", async (req, res) => {
                 users.id,
                 users.name,
                 users.uid,
-                classes.code AS shared_class
+                classes.code AS shared_class,
+                myavailability.day_of_week,
+                GREATEST(myavailability.start_time, theiravailability.start_time) AS shared_start_time,
+                LEAST(myavailability.end_time, theiravailability.end_time) AS shared_end_time
             FROM users
-            JOIN user_classes ON users.id = user_classes.user_id
-            JOIN classes ON user_classes.class_id = classes.id
+            JOIN user_classes 
+            ON users.id = user_classes.user_id
+            JOIN classes 
+            ON user_classes.class_id = classes.id
+            JOIN availability AS myavailability
+            ON myavailability.user_id = $1
+            JOIN availability AS theiravailability 
+            ON theiravailability.user_id = users.id
+            AND theiravailability.day_of_week = myavailability.day_of_week
+            AND theiravailability.start_time < myavailability.end_time
+            AND theiravailability.end_time > myavailability.start_time
             WHERE user_classes.class_id IN (
                 SELECT class_id
                 FROM user_classes
                 WHERE user_id = $1
             )
             AND users.id != $1
+<<<<<<< Updated upstream
+=======
+            AND NOT EXISTS (
+                SELECT 1
+                FROM blocked_users
+                WHERE 
+                    (blocker_id = $1 AND blocked_id = users.id)
+                    OR
+                    (blocker_id = users.id AND blocked_id = $1)
+            )
+                    ORDER BY users.name, classes.code, myavailability.day_of_week, shared_start_time
+>>>>>>> Stashed changes
             `,
             [userId]
         );
